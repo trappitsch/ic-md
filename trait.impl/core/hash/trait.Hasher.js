d4d3624139a465548c225f65ee9db9d4da6c16ee
trappitsch <trappitsch@users.noreply.github.com>
2025-07-30 13:31:44 +0000

    deploy: d7e784245922e0beb7c380d0a983aadb0339b740

--- trait.impl/core/hash/trait.Hasher.js
+++ trait.impl/core/hash/trait.Hasher.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rustc_hash",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"rustc_hash/struct.FxHasher.html\" title=\"struct rustc_hash::FxHasher\">FxHasher</a>"]]]]);
+    var implementors = Object.fromEntries([["rustc_hash",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"rustc_hash/struct.FxHasher.html\" title=\"struct rustc_hash::FxHasher\">FxHasher</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
